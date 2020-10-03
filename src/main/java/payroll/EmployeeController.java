d2ee14293be9a11c77be99a2a346e04803dd4bb6
Naveen Sundar <naveennvrgup@gmail.com>
2020-10-03 12:32:48 +0530

    feat: more rest with hateoas

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -2,6 +2,8 @@ package payroll;
 
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,37 +21,39 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all(){
+    List<Employee> all() {
         return repository.findAll();
     }
 
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee){
+    Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable long id){
-        return repository.findById(id)
-        .orElseThrow(()->new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EmployeeController.class).all())
+                        .withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
-    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable long id){
-        return repository.findById(id)
-        .map(employee -> {
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable long id) {
+        return repository.findById(id).map(employee -> {
             employee.setName(newEmployee.getName());
             employee.setRole(newEmployee.getRole());
             return repository.save(newEmployee);
-        })
-        .orElseGet(()->{
+        }).orElseGet(() -> {
             newEmployee.setId(id);
             return repository.save(newEmployee);
         });
     }
 
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable long id){
+    void deleteEmployee(@PathVariable long id) {
         repository.deleteById(id);
     }
 }
--- src/main/java/payroll/EmployeeNotFoundException.java
+++ src/main/java/payroll/EmployeeNotFoundException.java
@@ -1,6 +1,10 @@
 package payroll;
 
 public class EmployeeNotFoundException extends RuntimeException{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
     public EmployeeNotFoundException(long id) {
         super("Could not find employee"+id);
